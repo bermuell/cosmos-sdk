49172f1430e6e174499538a2ec1c510b020e20b1
Bernd Mueller <bernd@informal.systems>
2024-03-13 16:51:52 +0100

    Added megablocks header to BroadcastTx

--- client/broadcast.go
+++ client/broadcast.go
@@ -2,6 +2,7 @@ package client
 
 import (
 	"context"
+	"crypto/sha1"
 	"fmt"
 	"strings"
 
@@ -16,11 +17,22 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/tx"
 )
 
+func getMegablocksHeader(chainId string) []byte {
+	tx := []byte{0x23, 0x6d, 0x75, 0x78} //Megablocks MAGIC
+	// 4-bytes truncated sha1 checksum...
+	// TODO: check if we replace it with adler-32 !!!
+	checksum := sha1.Sum([]byte(chainId)) // checksum
+	megablocks_id := checksum[:4]
+	return append(tx, megablocks_id...)
+}
+
 // BroadcastTx broadcasts a transactions either synchronously or asynchronously
 // based on the context parameters. The result of the broadcast is parsed into
 // an intermediate structure which is logged if the context has a logger
 // defined.
 func (ctx Context) BroadcastTx(txBytes []byte) (res *sdk.TxResponse, err error) {
+	header := getMegablocksHeader(ctx.ChainID)
+	txBytes = append(header, txBytes...)
 	switch ctx.BroadcastMode {
 	case flags.BroadcastSync:
 		res, err = ctx.BroadcastTxSync(txBytes)
