f141287072c12d6344183546d77764513236cef0
Bernd Mueller <bernd@informal.systems>
2024-03-19 09:44:35 +0100

    add general chain-id flag to query commands

--- client/flags/flags.go
+++ client/flags/flags.go
@@ -108,6 +108,7 @@ func AddQueryFlagsToCmd(cmd *cobra.Command) {
 	cmd.Flags().Bool(FlagGRPCInsecure, false, "allow gRPC over insecure channels, if not the server must use TLS")
 	cmd.Flags().Int64(FlagHeight, 0, "Use a specific height to query state at (this can error if the node is pruning state)")
 	cmd.Flags().StringP(FlagOutput, "o", "text", "Output format (text|json)")
+	cmd.Flags().String(FlagChainID, "", "the chain ID to be used in query requests. Essential for Megablocks work correctly")
 
 	// some base commands does not require chainID e.g `simd testnet` while subcommands do
 	// hence the flag should not be required for those commands
